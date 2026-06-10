099ff3ee8e29c84739de4f95d42d338e74c26c6c
stasssokolovskiy <stasssokolovskiy@gmail.com>
2026-06-11 00:43:34 +0300

    Поменял названия колонок

--- generate.py
+++ generate.py
@@ -6,15 +6,15 @@ import sys
 NUM_ROWS = 50
 
 
-COLUMNS = ["COLUMN_1", "COLUMN_2", "COLUMN_3", "COLUMN_4"]
+COLUMNS = ["BEGIN_VERTEX", "WEIGHT", "END_VERTEX", "COLOR"]
 
 def generate_row():
 
     return {
-        "COLUMN_1": random.randint(0, 100),
-        "COLUMN_2": round(random.uniform(1.5, 9.9), 2),
-        "COLUMN_3": random.randint(0, 100),
-        "COLUMN_4": random.choice(["A", "B", "C"]),
+        "BEGIN_VERTEX": random.randint(0, 100),
+        "WEIGHT": round(random.uniform(1.5, 9.9), 2),
+        "END_VERTEX": random.randint(0, 100),
+        "COLOR": random.choice(["A", "B", "C"]),
     }
 
 OUTPUT_DIR = sys.argv[1] if len(sys.argv) > 1 else "/data"
