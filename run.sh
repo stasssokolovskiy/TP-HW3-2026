86fc1f792df001359ea2d48fa5b867533a2a4076
stasssokolovskiy <stasssokolovskiy@gmail.com>
2026-06-11 12:48:57 +0300

    Добавил Dockerfile для аналитика и команды

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@ case $1 in
     ("build_generator") docker build generator -t csv-generator;;
     ("run_generator") docker run -v .:/mnt csv-generator /mnt/data;;
     ("create_local_data") docker run -v .:/mnt csv-generator /mnt/Local_data;;
+    ("build_reporter") docker build analyst -t html-reporter;;
+    ("run_reporter") docker run -v ./data:/data html-reporter;;
     (*) echo "No such command";;
 esac
